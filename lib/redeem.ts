2db782f9c0152cd2e3d60a4e12cdb76f0d81f509
Will Kantaros <wkantaros@gmail.com>
2024-06-18 14:25:39 -0400

    update cheese orbit

--- lib/redeem.ts
+++ lib/redeem.ts
@@ -1,42 +1,39 @@
-import { setupArbitrumSdk } from './setupArbitrumSdk';
-import { L1TransactionReceipt } from '@arbitrum/sdk';
+import { setupArbitrumSdk } from './setupArbitrumSdk'
+import { L1TransactionReceipt } from '@arbitrum/sdk'
 import yargs from 'yargs'
-import { ethers } from 'ethers';
-require('dotenv').config();
+import { ethers } from 'ethers'
+require('dotenv').config()
 
 async function main() {
   // Hardcoded array of hashes
   const hashes = [
-    '0x6710886f80a9ca057e8829780e050b6a9695456ef4b4dbe55edac89feebd3303',
-    '0x68b6d635f4a48a0e966dcad238aeecb9558ccc2764c6f1bc3713e1924720fbd0',
-    '0x165e0baeaf6e44c615f72075c5b8cc6e0b755bdbd9edf7378e9fba6374c9db43',
-  ];
+    '0x54819b8da36314705287c784ce324ba048e3d463c23b609d11d32b4e48dea40a',
+    // '0x6710886f80a9ca057e8829780e050b6a9695456ef4b4dbe55edac89feebd3303',
+    // '0x68b6d635f4a48a0e966dcad238aeecb9558ccc2764c6f1bc3713e1924720fbd0',
+    // '0x165e0baeaf6e44c615f72075c5b8cc6e0b755bdbd9edf7378e9fba6374c9db43',
+  ]
 
-  const {
-    l1RpcProvider,
-    l2RpcProvider,
-    l2Network
-  } = await setupArbitrumSdk();
+  const { l1RpcProvider, l2RpcProvider, l2Network } = await setupArbitrumSdk()
 
-  const privateKey = process.env.PRIVATE_KEY as string;
-  const l1Signer = new ethers.Wallet(privateKey, l1RpcProvider);
-  const l2Signer = new ethers.Wallet(privateKey, l2RpcProvider);
+  const privateKey = process.env.PRIVATE_KEY as string
+  const l1Signer = new ethers.Wallet(privateKey, l1RpcProvider)
+  const l2Signer = new ethers.Wallet(privateKey, l2RpcProvider)
 
   for (const hash of hashes) {
-    const l1Receipt = await l1RpcProvider.getTransactionReceipt(hash);
-    const l1TransactionReceipt = new L1TransactionReceipt(l1Receipt);
-    const messages = await l1TransactionReceipt.getL1ToL2Messages(l2Signer);
-    const l1ToL2Msg = messages[0];
+    const l1Receipt = await l1RpcProvider.getTransactionReceipt(hash)
+    const l1TransactionReceipt = new L1TransactionReceipt(l1Receipt)
+    const messages = await l1TransactionReceipt.getL1ToL2Messages(l2Signer)
+    const l1ToL2Msg = messages[0]
 
-    const tx = await l1ToL2Msg.redeem();
-    const { transactionHash: redeemHash } = await tx.wait();
-    console.log(`${hash},${redeemHash}`);
+    const tx = await l1ToL2Msg.redeem()
+    const { transactionHash: redeemHash } = await tx.wait()
+    console.log(`${hash},${redeemHash}`)
   }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main().catch(error => {
+  console.error(error)
+  process.exitCode = 1
+})
--- lib/setupArbitrumSdk.ts
+++ lib/setupArbitrumSdk.ts
@@ -1,91 +1,99 @@
-import { addCustomNetwork, getL2Network, L1Network, L2Network } from "@arbitrum/sdk";
-import { ethers } from 'ethers';
+import {
+  addCustomNetwork,
+  getL2Network,
+  L1Network,
+  L2Network,
+} from '@arbitrum/sdk'
+import { ethers } from 'ethers'
 
 export enum ChainId {
   BASE = 8453,
   DEGEN = 666666666,
+  CHEESE = 383353,
+  ARBITRUM = 42161,
 }
 
 const rpcURLs: Record<ChainId, string | null> = {
   [ChainId.BASE]: 'https://mainnet.base.org/',
   [ChainId.DEGEN]: 'https://rpc.degen.tips/http',
-};
+  [ChainId.CHEESE]: 'https://cheesechain.calderachain.xyz/http',
+  [ChainId.ARBITRUM]: 'https://arb1.arbitrum.io/rpc',
+}
 
-const getRpcUrl = (chainId: ChainId) => rpcURLs[chainId];
+const getRpcUrl = (chainId: ChainId) => rpcURLs[chainId]
 
-const setupBaseDegen = () => {
+const setupParentChild = (parent: ChainId, child: ChainId) => {
   const customL1Network: L1Network = {
     blockTime: 10,
-    chainID: ChainId.BASE,
+    chainID: parent,
     explorerUrl: '',
     isCustom: true,
-    name: 'Base',
-    partnerChainIDs: [ChainId.DEGEN],
+    name: 'Arbitrum',
+    partnerChainIDs: [child],
     isArbitrum: false,
   }
 
   const customL2Network: L2Network = {
-    chainID: ChainId.DEGEN,
-    confirmPeriodBlocks: 20,
+    chainID: ChainId.CHEESE,
+    confirmPeriodBlocks: 25,
     ethBridge: {
-      bridge: '0xEfEf4558802bF373Ce3307189C79a9cAb0a4Cb9C',
-      inbox: '0x21A1e2BFC61F30F2E81E0b08cd37c1FC7ef776E7',
-      outbox: '0xe63ddb12FBb6211a73F12a4367b10dA0834B82da',
-      rollup: '0xD34F3a11F10DB069173b32d84F02eDA578709143',
-      sequencerInbox: '0x6216dD1EE27C5aCEC7427052d3eCDc98E2bc2221',
+      bridge: '0xA337997ab18164Dfe1e8A94E8D912e8d4e2ce173',
+      inbox: '0x58e3F0ed71ac29501326aeE9564674E43812cc24',
+      outbox: '0x9bAA9C2c313510D4f3CB2673b2f2603ED45d6eDd',
+      rollup: '0xb9E6B5AcB523D431f6D136C56d371271456757E7',
+      sequencerInbox: '0x714d792CB8BFB9F70Cde071904d8743280267ab2',
     },
-    explorerUrl: 'https://explorer.degen.tips',
+    explorerUrl: 'https://cheesechain.calderaexplorer.xyz',
     isArbitrum: true,
     isCustom: true,
-    name: 'Degen',
-    partnerChainID: ChainId.BASE,
+    name: 'Cheese',
+    partnerChainID: ChainId.ARBITRUM,
     retryableLifetimeSeconds: 7 * 24 * 60 * 60,
     nitroGenesisBlock: 0,
     nitroGenesisL1Block: 0,
     depositTimeout: 900000,
     tokenBridge: {
-      l1CustomGateway: '0x457fb5fC5dcE35cBf644e36eFD13566574CB5171',
-      l1ERC20Gateway: '0xBeeC380b80c1679Cdc73805a1F809c8DcE31C680',
-      l1GatewayRouter: '0x7cFA460bBA19C712Ce5e3a199B8dFCd5E730E6FB',
-      l1MultiCall: '0x24107E688696CE56e94635AB8c008E187C3C0702',
-      l1ProxyAdmin: '0xFB48D385Fa3da33762B350e1d705b9E46054E677',
+      l1CustomGateway: '0x762AE18EDA279709C106eC76d4b3985366D87C3F',
+      l1ERC20Gateway: '0x4D692d1B2E5Cef2A6092de572231D54D07691aA0',
+      l1GatewayRouter: '0xA4BD9786c885EB99Cb6C6886d8f9CE8d96cE99F6',
+      l1MultiCall: '0x909b042B88F587d745dBF52e2569545376f6eAA4',
+      l1ProxyAdmin: '0xB9b62DF4C8F4605E0772d05506203D7bd4d2F968',
       l1Weth: '0x0000000000000000000000000000000000000000',
       l1WethGateway: '0x0000000000000000000000000000000000000000',
-      l2CustomGateway: '0x2964b500c584b363DA9F0d38eaAd76b4Ab319143',
-      l2ERC20Gateway: '',
-      l2GatewayRouter: '0xD971e60b7eE1BEddE5B48F644a342A78E1535452',
-      l2Multicall: '0x5304b5DbBfCe2fb40AE11Ed51E70699FC1F25fC9',
-      l2ProxyAdmin: '0x2Ef661C018E0B6eD1C7fd214097Fc04A30F19088',
+      l2CustomGateway: '0x1Dd3e9AbA3A50D071C250356551c774B5F28B567',
+      l2ERC20Gateway: '0x8737E402817e5B82B0ED43dC791e4E260e00c9d5',
+      l2GatewayRouter: '0x1E7090DCA131C7DE551becE4eCd2eBD5a37Aff0D',
+      l2Multicall: '0x36528Af3c5Ef49fDbB87EcB87027c98DeeD57a00',
+      l2ProxyAdmin: '0xe63cE9FeF9FC31FfcbD54D1aCfFEAcAf737e2626',
       l2Weth: '0x0000000000000000000000000000000000000000',
-      l2WethGateway: '0x0000000000000000000000000000000000000006',
+      l2WethGateway: '0x0000000000000000000000000000000000000000',
     },
     blockTime: 0.25,
     partnerChainIDs: [],
-  };
+  }
 
   try {
     addCustomNetwork({
-      customL1Network,
       customL2Network,
     })
   } catch (e) {
-    console.log("Failed to add custom network", e['message']);
-  };
+    console.log('Failed to add custom network', e['message'])
+  }
 }
 
 export const setupArbitrumSdk = async () => {
-  setupBaseDegen();
+  setupParentChild(ChainId.ARBITRUM, ChainId.CHEESE)
 
-  const l1Url = getRpcUrl(ChainId.BASE);
-  const l2Url = getRpcUrl(ChainId.DEGEN);
+  const l1Url = getRpcUrl(ChainId.ARBITRUM)
+  const l2Url = getRpcUrl(ChainId.CHEESE)
 
-  if (!l1Url) throw new Error('No rpc for L1');
-  if (!l2Url) throw new Error('No rpc for the specified L2 chainId');
+  if (!l1Url) throw new Error('No rpc for L1')
+  if (!l2Url) throw new Error('No rpc for the specified L2 chainId')
 
-  const l1RpcProvider = new ethers.providers.JsonRpcProvider(l1Url);
-  const l2RpcProvider = new ethers.providers.JsonRpcProvider(l2Url);
+  const l1RpcProvider = new ethers.providers.JsonRpcProvider(l1Url)
+  const l2RpcProvider = new ethers.providers.JsonRpcProvider(l2Url)
 
-  const l2Network = await getL2Network(l2RpcProvider);
+  const l2Network = await getL2Network(l2RpcProvider)
 
   return {
     l1RpcProvider,
